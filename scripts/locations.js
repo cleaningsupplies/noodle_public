723673f8994598d2c5c1a65f098a651cae08a430
steffi <stephanielindner@outlook.de>
2022-07-15 20:39:55 +0200

    simple layout and functions

---
+++ scripts/locations.js
@@ -0,0 +1,32 @@
+const locations = document.querySelectorAll(".locations");
+const location_elements = document.querySelector(".locations").children;
+
+const cities = [];
+cities.push(document.querySelector(".cityA"), 
+               document.querySelector(".cityB"),
+               document.querySelector(".cityC"),
+               document.querySelector(".cityD"));
+
+locations.forEach(location => location.addEventListener("click", switchLocation));
+
+function switchLocation(event){
+     let city = event.target;
+     removeActive();
+     city.classList.add("active");
+     switch(city.id){
+          case "cityA": cities[0].style.display = ""; break;
+          case "cityB": cities[1].style.display = ""; break;
+          case "cityC": cities[2].style.display = ""; break;
+          case "cityD": cities[3].style.display = ""; break;
+     }
+
+}
+
+function removeActive(){
+     for(let i = 0; i < location_elements.length; i++){  
+          cities[i].style.display = "none";
+          if(location_elements[i].className === "active"){
+               location_elements[i].classList.remove("active");
+           }
+     }
+}
